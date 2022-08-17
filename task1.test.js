be23ad9ce7a6bcfb4543fa532013215c364734c8
AlucardSanin <alucardsanin@gmail.com>
2022-08-17 15:15:15 -0400

    Passed

--- task1.test.js
+++
@@ -1,5 +0,0 @@
-const stringLength = require('./task1')
-
-test('Length of string longer than 1 and lower or equal to 10', () => {
-    expect(stringLength('apple')).toBeTruthy();
-})
---
+++ task2.test.js
@@ -0,0 +1,5 @@
+const reverseString = require('./task2')
+
+test('Reverse', () => {
+    expect(reverseString('apples')).toBe("selppa");
+})
