0014d93ddeb14bc7b7bfc81e5cbbf96ec7469990
AlucardSanin <alucardsanin@gmail.com>
2022-08-19 12:11:13 -0400

    Task 3 Solved

--- task2.test.js
+++ task2.test.js
@@ -1,6 +1,49 @@
-const calculator= require('./task3')
+const { Calculator } = require('./task3')
 
-test('Solved', (a=5, b=5) => {
-    expect(calculator.sum('')).toBe("");
+describe("Calculator Add Tests", ()=> {
+  test('Expected 5+2 = 7', () => {
+      expect(Calculator.Add(5,2)).toBe(7);
+  })
+  test('Expected a defined result', () => {
+    expect(Calculator.Add(5,2)).toBeDefined();
+  })
+  test('Expected to be a Number', () => {
+    expect(typeof Calculator.Add(5,2)).toBe("number")
+  })
 })
 
+describe("Calculator Substract Tests", ()=> {
+  test('Expected 5-2 = 3', () => {
+      expect(Calculator.Sub(5,2)).toBe(3);
+  })
+  test('Expected a defined result', () => {
+    expect(Calculator.Sub(5,2)).toBeDefined();
+  })
+  test('Expected to be a Number', () => {
+    expect(typeof Calculator.Sub(5,2)).toBe("number")
+  })
+})
+
+describe("Calculator Divide Tests", ()=> {
+  test('Expected 10/2 = 5', () => {
+      expect(Calculator.Div(10,2)).toBe(5);
+  })
+  test('Expected a defined result', () => {
+    expect(Calculator.Div(5,2)).toBeDefined();
+  })
+  test('Expected to be a Number', () => {
+    expect(typeof Calculator.Sub(5,2)).toBe("number")
+  })
+})
+
+describe("Calculator Multiply Tests", ()=> {
+  test('Expected 10*2 = 20', () => {
+      expect(Calculator.Mul(10,2)).toBe(20);
+  })
+  test('Expected a defined result', () => {
+    expect(Calculator.Div(5,2)).toBeDefined();
+  })
+  test('Expected to be a Number', () => {
+    expect(typeof Calculator.Sub(5,2)).toBe("number")
+  })
+})
--- task3.js
+++ task3.js
@@ -1,8 +1,6 @@
-const calcultor (a,b) = {
-    add: (a+b),
-    sub: (a-b),
-    div: (a/b),
-    mul: (a*b),    
+ export class Calculator {
+   static Add = (a,b) => (a+b); 
+   static Sub = (a,b) => (a-b);
+   static Div = (a,b) => (a/b);  
+   static Mul = (a,b) => (a*b); 
 }
-
-module.exports = reverseString;
